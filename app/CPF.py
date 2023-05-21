526a3138b340432cba7e4be0b8b557aa26af2b23
Amanda Gomes <amandaferreirak10@gmail.com>
2023-05-21 18:33:34 -0300

    mais alguns ajustes nas docstrings das funções e remoção de códigos comentados.

--- app/CPF.py
+++ app/CPF.py
@@ -1,8 +1,7 @@
 import re
 
 def validate_input_doc_num(doc_num: str) -> str:
-    """
-    Valida a entrada do documento.
+    """Valida a entrada do documento.
 
     Args:
         doc_num (str): string que contém o número do documento.
@@ -18,7 +17,8 @@ def validate_input_doc_num(doc_num: str) -> str:
         elif pattern == False:
             pass
         else:
-            raise ValueError('O CPF deve conter 11 dígitos e deve ser inserido em um dos dois formatos a seguir:\n 1 -> 000.000.000-00\n 2-> 00000000000')
+            raise ValueError("O CPF deve conter 11 dígitos e deve ser inserido em um dos dois"
+                             "formatos a seguir:\n 1 -> 000.000.000-00\n 2-> 00000000000")
     if repeated_digits(doc_num):
         raise ValueError('O CPF não pode conter 11 dígitos repetidos!')
 
@@ -26,15 +26,16 @@ def validate_input_doc_num(doc_num: str) -> str:
 
 
 def have_mask(doc_num: str) -> bool:
-    """
-    Encontra o padrão da string inserida. 
+    """Encontra o padrão da string inserida.
+
     Dois padrões são válidos:
-        1 - 000.000.000-00 (com máscara)
-        2 - 00000000000 (sem máscara)
-    :param str: doc_num -> string que contém o número do documento (CPF).
-    :return bool: True or False. Depende, se tiver no padrão de máscara, retorna True.
+        1 - 000.000.000-00 (com máscara).
+        2 - 00000000000 (sem máscara).
+    Args:
+        - doc_num (str): string que contém o número do documento (CPF).
+    Returns:
+        (bool) True or False. Depende, se tiver no padrão de máscara, retorna True.
     """
-    # doc_num: str = validate_input_doc_num(doc_num)
     tam_string: int = len(doc_num)
     if tam_string == 11:
         cpf_pattern_2: str = '[0-9]{11}'
@@ -47,35 +48,39 @@ def have_mask(doc_num: str) -> bool:
 
 
 def repeated_digits(doc_num: str) -> bool:
+    """Verifica se o documento informado contém apenas dígitos repetidos.
+
+    Exemplo: 000.000.000-00 ou 00000000000.
+    Args:
+        - doc_num (str): string com o número do documento (CPF).
+    Returns:
+        (bool): caso contenha apenas dígitos repetidos.
     """
-    Verifica se o documento informado contém apenas dígitos repetidos.
-    Exemplo: 000.000.000-00 ou 00000000000
-    :param doc_num: string com o número do documento (CPF).
-    :return True/False: caso contenha apenas dígitos repetidos
-    """
-    # doc_num = validate_input_doc_num(doc_num)
     return len(set(list(doc_num))) == 1
 
 
 def create_mask(doc_num: str) -> str:
+    """Cria a máscara no formato ___.___.___-__.
+
+    Args:
+        doc_num (str): string com o número do documento.
+    Returns:
+        doc_num (str): retorna o cpf com máscara.
+        Exemplo: 012.345.678-90 
     """
-    Cria a máscara no formato ___.___.___-__
-    :param doc_num: string com o número do documento.
-    :return: exemplo: 012.345.678-90 
-    """
-    # doc_num = validate_input_doc_num(doc_num)
     if not have_mask(doc_num):
         doc_num = f'{doc_num[:3]}.{doc_num[3:6]}.{doc_num[6:9]}-{doc_num[9:]}'
     return doc_num
 
 
 def remove_mask(doc_num: str) -> str:
+    """Remove os caracteres '.' e '-'.
+    
+    Args:
+        - doc_num (str): string com os números do documento.
+    Returns:
+        (str): string com os números apenas, sem a máscara.
     """
-    Remove os caracteres '.' e '-'.
-    :param doc_num: string com os números do documento.
-    :return:
-    """
-    # doc_num = validate_input_doc_num(doc_num)
     if not have_mask(doc_num):
         return doc_num
     
@@ -83,19 +88,18 @@ def remove_mask(doc_num: str) -> str:
 
 
 def validate(doc_num: str) -> bool:
+    """Verifica se os dois últimos dígitos do CPF são válidos.
+
+    Args:
+        - doc_num (str): string com os números do documento (CPF).
+    Returns:
+        (bool): se os dois últimos dígitos verificados estiverem corretos, retorna True.
     """
-    Verifica se os dois últimos dígitos do CPF são válidos. 
-    :param doc_num: string com os números do documento (CPF).
-    :return True/False: se os dois últimos dígitos verificados estiverem corretos, retorna True.
-    """
-    # doc_num = validate_input_doc_num(doc_num)
-    # if repeated_digits(doc_num):
-    #     return False
-    
+
     doc_num: str = validate_input_doc_num(doc_num)
 
     nums_cpf: list = list(doc_num[::-1])[2:]
-    for i in list(range(0, 2)):
+    for _ in list(range(0, 2)):
         to_sum: int = sum([enum * int(num) for enum, num in enumerate(nums_cpf, start=2)])
         quociente, resto =  divmod(to_sum, 11)
 
